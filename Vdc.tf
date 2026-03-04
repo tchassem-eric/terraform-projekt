b95687304bebb243b93424fdf19b84e909f9c582
tchassem-eric <60570884+tchassem-eric@users.noreply.github.com>
2026-03-04 15:11:16 +0100

    Update Vdc.tf

--- Vdc.tf
+++ Vdc.tf
@@ -1,9 +1,8 @@
-resource "ionoscloud_datacenter" "my_vdc" {
-  name        = "my-vdc"
-  location    = "de/fra"
+resource "ionoscloud_datacenter" "proditech_vdc" {
+  name        = "proditech-vdc"
+  location    = "de/fra"   
   description = "VDC Kubernetes Cluster"
 }
-
 
 output "vdc_id" {
   value = ionoscloud_datacenter.my_vdc.id
