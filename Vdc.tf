304ab9b7b0c23cab019258cfa4a1cadb8937a612
tchassem-eric <60570884+tchassem-eric@users.noreply.github.com>
2026-03-04 15:00:47 +0100

    Create module  Vdc.tf

---
+++ Vdc.tf
@@ -0,0 +1,10 @@
+resource "ionoscloud_datacenter" "my_vdc" {
+  name        = "my-vdc"
+  location    = "de/fra"
+  description = "VDC Kubernetes Cluster"
+}
+
+
+output "vdc_id" {
+  value = ionoscloud_datacenter.my_vdc.id
+}
