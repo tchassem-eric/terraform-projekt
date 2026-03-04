f51db7fbf94e4b10133a012addcf68c1ef88dce7
tchassem-eric <60570884+tchassem-eric@users.noreply.github.com>
2026-03-04 15:13:59 +0100

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -3,22 +3,22 @@ variable "datacenter_id" {
 }
 
 resource "ionoscloud_kubernetes_cluster" "my_cluster" {
-  name              = "my-cluster"
-  datacenter_id     = var.datacenter_id
+  name               = "my-cluster"
+  datacenter_id      = var.datacenter_id
   kubernetes_version = "1.29.0"
 }
 
 resource "ionoscloud_kubernetes_node_pool" "my_nodepool" {
-  cluster_id       = ionoscloud_kubernetes_cluster.my_cluster.id
-  name             = "my-nodepool"
-  cpu_cores        = 4
-  ram              = 8192
-  storage_type     = "HDD"
-  storage_size     = 50
+  cluster_id        = ionoscloud_kubernetes_cluster.my_cluster.id
+  name              = "my-nodepool"
+  cpu_cores         = 4
+  ram               = 8192
+  storage_type      = "HDD"
+  storage_size      = 50
   availability_zone = "AUTO"
-  min_nodes        = 1
-  max_nodes        = 3
-  initial_nodes    = 1
+  min_nodes         = 1
+  max_nodes         = 3
+  initial_nodes     = 1
 }
 
 output "k8s_cluster_id" {
